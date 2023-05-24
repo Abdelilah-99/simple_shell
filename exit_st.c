319c8d3462d0a968c2e553c10325e73dea63cff8
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-05-24 16:54:05 +0100

    fix

--- exit_st.c
+++ exit_st.c
@@ -1,36 +1,4 @@
 #include "shell.h"
-
-/**
- * _atoi - convesrt string to an integer
- * @s: string
- * Return: returns an integer
- */
-int _atoi(char *s)
-{
-	int i = 0, signe = 1, c = 0;
-	unsigned int r = 0;
-
-	while (s[i])
-	{
-		if (s[i] == '-')
-		{
-			signe *= -1;
-		}
-		while (s[i] >= '0' && s[i] <= '9')
-		{
-			c = 1;
-			r = (r * 10) + (s[i] - '0');
-			i++;
-		}
-		if (c == 1)
-		{
-			break;
-		}
-		i++;
-	}
-	r *= signe;
-	return (r);
-}
 /**
  * print_exit - exit function
  */
